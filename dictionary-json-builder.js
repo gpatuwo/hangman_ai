606aae16c77b2b6346cedc0e7786091fb7811e0c
Grace Patuwo <grace.patuwo@gmail.com>
2017-05-31 15:51:48 -0700

    renamed to json dict

---
+++ dictionary-json-builder.js
@@ -0,0 +1,44 @@
+/*
+script to build json dictionary from dictionary txt files
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+let wordsHash = {};
+
+function createInitialHashDictionary() {
+
+  let length = 1;
+  let path = `./dictionary-txt/${length}-letter-words.txt`;
+  /* data is a buffer, need to convert to usable data(string).
+  O(num of words in list) time + space */
+  fs.readFile(path, "utf8", function cb(err, data){
+    let wordsArr = data.split('\n');
+
+    // loop to create hash of arr of word's letters as val
+    // under-index to discard linebreak char
+    for (var j = 0; j < wordsArr.length - 1; j++) {
+      let word = wordsArr[j];
+      wordsHash[j] = word.split('');
+    }
+
+    console.log(wordsHash);
+
+    let endPath =  `./dictionary-json/${length}-letter.json`;
+
+    fs.writeFileSync(endPath, JSON.stringify(wordsHash, null, 2) , 'utf-8');
+
+    // loop thru length dictionary files
+    if (length < 24) {
+      length++;
+      path = `./dictionary-txt/${length}-letter-words.txt`;
+      fs.readFile(path, "utf8", cb);
+    } else {
+      console.log(`created ${length} lists`);
+    }
+  });
+}
+
+createInitialHashDictionary();
