c8365d6733d2a4abc71d1f3432a48293d13d74fc
Grace Patuwo <grace.patuwo@gmail.com>
2017-05-31 15:50:36 -0700

    created hash dictionaries

--- dictionary-hash-builder.js
+++ dictionary-hash-builder.js
@@ -9,30 +9,36 @@ const fs = require('fs');
 let wordsHash = {};
 
 function createInitialHashDictionary() {
-  // loop thru length dictionary files
-  for (var length = 1; length < 25; length++) {
-    let path = `./dictionary-txt/${length}-letter-words.txt`;
 
-    /* data is a buffer, need to convert to usable data(string).
-    O(num of words in list) time + space */
-    fs.readFile(path, "utf8", (err, data) => {
-      let wordsArr = data.split('\n');
+  let length = 1;
+  let path = `./dictionary-txt/${length}-letter-words.txt`;
+  /* data is a buffer, need to convert to usable data(string).
+  O(num of words in list) time + space */
+  fs.readFile(path, "utf8", function cb(err, data){
+    let wordsArr = data.split('\n');
 
-      // loop to create hash of arr of word's letters as val
-      // under-index to discard linebreak char
-      for (var j = 0; j < wordsArr.length - 1; j++) {
-        let word = wordsArr[j];
-        wordsHash[j] = word.split('');
-      }
+    // loop to create hash of arr of word's letters as val
+    // under-index to discard linebreak char
+    for (var j = 0; j < wordsArr.length - 1; j++) {
+      let word = wordsArr[j];
+      wordsHash[j] = word.split('');
+    }
 
-      console.log(wordsHash);
+    console.log(wordsHash);
 
-      let endPath =  `./dictionary-hash/${length}-letter.json`;
+    let endPath =  `./dictionary-hash/${length}-letter.json`;
 
-      fs.writeFileSync(endPath, JSON.stringify(wordsHash, null, 2) , 'utf-8');
-    });
-  }
+    fs.writeFileSync(endPath, JSON.stringify(wordsHash, null, 2) , 'utf-8');
 
+    // loop thru length dictionary files
+    if (length < 24) {
+      length++;
+      path = `./dictionary-txt/${length}-letter-words.txt`;
+      fs.readFile(path, "utf8", cb);
+    } else {
+      console.log(`created ${length} lists`);
+    }
+  });
 }
 
 createInitialHashDictionary();
